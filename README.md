63df3d90e33dae0ea8f291b0d922b6f1529044d7
Durga Prasad Narikalapa <ndurgaprasad10@gmail.com>
2022-07-30 18:08:21 +0530

    setting up the react-webpack-template

---
+++ README.md
@@ -0,0 +1,15 @@
+# react-starter-template
+
+## Start
+npm start
+
+## Build in Production
+npm run build
+
+## Build in Dev
+npm run build-dev
+
+## Clean project directory
+npm run clean
+
+
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles/index.scss";
+
+const App = () => {
+  return (
+    <main>
+      <section>
+        <h1>Hi, React</h1>
+      </section>
+    </main>
+  );
+};
+
+export default App;
